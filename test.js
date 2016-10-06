3834702b9c2b88ca4e907a0229bc10f17723d6ba
Milk <2582789473@qq.com>
2016-10-06 14:44:42 +0800

    增加了test.js

---
+++ test.js
@@ -0,0 +1,24 @@
+function sayhello(){
+	alert("sayheiio");
+};
+
+function hey (name ) {
+	
+
+
+}
+function nihaoa(nishibushi) {
+	console.log("hello");
+
+	function buda(zheyang) {
+		assert.deepEqual(na, bushijiang);		
+	}
+
+
+}
+
+var wing.addEventListener('eing', function(e) {
+	eing;
+});	
+
+
